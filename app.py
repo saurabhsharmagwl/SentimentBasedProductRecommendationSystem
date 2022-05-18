23d8776066026cc1fed6d2c526429944fe97b502
saurabhsharmagwl <saurabhsharmagwl@gmail.com>
2022-05-18 22:06:24 +0530

    uploading file

--- app.py
+++ app.py
@@ -31,15 +31,6 @@ def prediction():
         return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
 
 
-@app.route('/predictSentiment', methods=['POST'])
-def predict_sentiment():
-    # get the review text from the html form
-    review_text = request.form["reviewText"]
-    print(review_text)
-    pred_sentiment = sentiment_model.classify_sentiment(review_text)
-    print(pred_sentiment)
-    return render_template("index.html", sentiment=pred_sentiment)
-
 
 if __name__ == '__main__':
     app.run()
